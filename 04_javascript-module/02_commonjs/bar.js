e3832a454d7e5c061f2800943d7b9fb52bab69f8
u8x9 <u8x@eclipso.eu>
2021-02-10 16:39:10 +0800

    node中module.exports和exports的关系

--- 04_javascript-module/02_commonjs/bar.js
+++ 04_javascript-module/02_commonjs/bar.js
@@ -23,3 +23,4 @@ exports.sayHello = sayHello;
 // node 通过 module.export 实现这个规范
 // 本质上是 module.exports 在导出
 // module.exports = exports = 目标文件的require的对象
+// 一旦 module.exports 赋予了和 exports 不同的对象，两者之间的关联关系也中断了，但最终 require 还是以 module.exports 为主。因为，node中，本质是通过 module.exports 导出
